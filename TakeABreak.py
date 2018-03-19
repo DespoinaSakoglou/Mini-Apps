b77a62643e7d4808baf6cd10f9eb70fec3bbe1f8
DespoinaSakoglou <37550730+DespoinaSakoglou@users.noreply.github.com>
2018-03-19 18:22:18 -0400

    Take a Break
    An app designed to remind the user to take a break from work every 2 hours for a total of 3 breaks. It automatically opens a YouTube video at each break.

---
+++ TakeABreak.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 19 13:15:35 2018
+
+@author: Despoina
+"""
+
+import time
+import webbrowser
+
+total_breaks = 3
+total_count = 0
+
+print("This program starts on: "+time.ctime())
+while (total_count < total_breaks):
+    time.sleep(10)
+    webbrowser.open("https://www.youtube.com/watch?v=hiVyZkR6uPk&list=RDhiVyZkR6uPk")
+    total_count=total_count+1
