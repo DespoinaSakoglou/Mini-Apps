526f5fac4112a716e90d4b8c0a771c60335ed731
DespoinaSakoglou <37550730+DespoinaSakoglou@users.noreply.github.com>
2018-03-20 18:56:50 -0400

    Updated comments
    Added program description and explained what each line of code does when executed.

--- TakeABreak.py
+++ TakeABreak.py
@@ -1,18 +1,29 @@
-# -*- coding: utf-8 -*-
+
 """
+A program that plays a youtube video every two hours for
+three consecutive breaks.
+
 Created on Mon Mar 19 13:15:35 2018
 
 @author: Despoina
 """
 
+# import time module to use: 1)current time-shows program start 2)sleep-adds 2-hour intervals between breaks
 import time
+# import webbrowser-opens the youtube video
 import webbrowser
 
+# set number of total_breaks 
 total_breaks = 3
 total_count = 0
 
+# print out start time of program
 print("This program starts on: "+time.ctime())
+# use a while loop to repeat the break 3 times
 while (total_count < total_breaks):
+    #pause the program for 2 hours (in seconds)
     time.sleep(2*60*60)
-    webbrowser.open("https://www.youtube.com/watch?v=hiVyZkR6uPk&list=RDhiVyZkR6uPk")
+    #after 2 hours, open the youtube video
+    webbrowser.open("https://www.youtube.com/watch?v=o3NndBTqC3Y")
+    #increment counter for next iteration
     total_count=total_count+1
